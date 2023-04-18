df9c4fc18304ae6723ebf0bd4c014b9ec4705a55
Diego Mercado <diego.mercado1997@hotmail.com>
2023-04-18 10:07:04 -0300

    agregamos maquetado

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+let btnThemeLight = document.querySelector("#btnThemeLight");
+let btnThemeDark = document.querySelector("#btnThemeDark");
+
+btnThemeLight.addEventListener("click", () => cambiarTema("light"));
+btnThemeDark.addEventListener("click", () => cambiarTema("dark"));
+
+//leer el localStorage
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+console.log(temaConfigurado)
+cambiarTema(temaConfigurado)
+
+function cambiarTema(color) {
+  console.log(color);
+  //data-bs-theme = 'light/dark'
+  document.querySelector("html").setAttribute("data-bs-theme", color);
+  //guardar en Localstorage
+  localStorage.setItem('tema', JSON.stringify(color));
+}
