41c21f0507481207024c2697ce437b2f5d6154e2
Diego Mercado <diego.mercado1997@hotmail.com>
2023-04-18 11:00:04 -0300

    agregamos uuid a la clase pelicula

--- js/administrador.js
+++ js/administrador.js
@@ -1,4 +1,16 @@
 import Pelicula from "./classPelicula.js";
 
-let nuevaPelicula = new Pelicula(0,"supermario","algunades","-","url de imagen","aventura",20023,93,"EEUU","variosactores")
-console.log(nuevaPelicula)
+let formPelicula = document.getElementById("formPelicula")
+formPelicula.addEventListener("submit",prepararFormulario)
+
+
+function prepararFormulario(e) {
+    e.preventDefault();
+    crearPelicula()
+}
+
+
+function crearPelicula() {
+    let nuevaPelicula = new Pelicula("supermario","algunades","-","url de imagen","aventura",20023,93,"EEUU","variosactores")
+    console.log(nuevaPelicula)
+}
