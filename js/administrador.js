85843fe2c8526816e88817ec8f544264cb0a947f
Diego Mercado <diego.mercado1997@hotmail.com>
2023-04-18 10:39:37 -0300

    agregamos el prototipo pelicula y creamos un objeto nueva peli

---
+++ js/administrador.js
@@ -0,0 +1,4 @@
+import Pelicula from "./classPelicula.js";
+
+let nuevaPelicula = new Pelicula(0,"supermario","algunades","-","url de imagen","aventura",20023,93,"EEUU","variosactores")
+console.log(nuevaPelicula)
